b5bde536687bfa380966851a0a88383d19777db4
Pierre Morel <pier.morel@gmail.com>
2026-03-17 16:01:30 +0100

    Renamed files for easier navigation and merged back a few examples that belong together

--- gramm/examples/example_advanced_superimposed_plots.m
+++ gramm/examples/example_advanced_superimposed_plots.m
@@ -2,10 +2,10 @@
 
 % Shared variable
 % We stat by loading the sample data (structure created from the carbig
-% dataset));
+% dataset)
 load example_data;
 
-%% Superimposing gramm plots with update(): Plotting all the data in the background of facets
+%% Superimposing plots: plotting all the data in the background of facets
 %Inspired by https://drsimonj.svbtle.com/plotting-background-data-for-groups-with-ggplot2
 
 load fisheriris.mat
@@ -46,3 +46,32 @@ g(2,1).no_legend();
 g.axe_property('TickDir','out','XGrid','on','Ygrid','on','GridColor',[0.5 0.5 0.5]);
 %Draw the news elements
 g.draw();
+
+
+%% Superimposing plots: using different groups for different layers
+% By using the method update() after a first draw() call of a gramm object,
+% it is possible to add or remove grouping variables.
+% Here in a first gramm plot we make a glm fit of cars Acceleration as a
+% function of Horsepower, across all countries and number of cylinders, and
+% change the color options so that the fit appears in grey
+
+clear g10
+figure;
+g10=gramm('x',cars.Horsepower,'y',cars.Acceleration,'subset',cars.Cylinders~=3 & cars.Cylinders~=5);
+g10.set_names('color','# Cylinders','x','Horsepower','y','Acceleration','Column','Origin');
+g10.set_color_options('chroma',0,'lightness',30);
+g10.stat_glm('geom','area','disp_fit',false);
+g10.set_title('Update example'); %Title must be provided before the first draw() call
+g10.draw();
+snapnow;
+
+% After the first draw() call (optional), we call the update() method by specifying a
+% new grouping variable determining colors. We also change the facet_grid()
+% options, which will duplicate the fit made earlier across all new facets.
+% Last, color options are reinitialized to default values
+
+g10.update('color',cars.Cylinders);
+g10.facet_grid([],cars.Origin_Region);
+g10.set_color_options();
+g10.geom_point();
+g10.draw();
--- gramm/examples/example_base_repeated_measurements.m
+++ gramm/examples/example_base_repeated_measurements.m
@@ -11,7 +11,7 @@ load example_data;
 % trajectories. Here for example we generate 50 trajectories, each of
 % length 40. The grouping data is then given per trajectory and not per
 % data point. Here the color grouping variable is thus given as a 1x50
-    % cellstr.
+% cellstr.
 
 %We generate 50 trajectories of length 40, with 3 groups
 N=50;
@@ -38,11 +38,46 @@ g(2,1).stat_smooth();
 g(2,1).set_point_options('base_size',3);
 g(2,1).set_title('stat_smooth()');
 
-
 g(2,2).stat_summary();
 g(2,2).set_title('stat_summary()');
 
 g.set_title('Visualization of repeated trajectories ');
+
+figure;
+g.draw();
+
+
+%% Methods for visualizing repeated event densities (e.g. spike trains)
+% With the support of 2D inputs for X and gramm's functionality for
+% representing the density of data, useful neuroscientific plots can be
+% generated when the provided X corresponds to spike trains: raster plots
+% and peristimulus time histograms (PSTHs).
+
+%We generate 50 spike trains, with 3 groups
+N=50;
+cval={'A' 'B' 'C'};
+cind=randi(3,N,1);
+c=cval(cind);
+train_template=[zeros(1,300)  ones(1,200)];
+%Pseudo-poisson spike trains
+spike_train=cell(N,1);
+for k=1:N
+    temp_train=rand*0.05+train_template/(cind(k)*8);
+    U=rand(size(temp_train));
+    spike_train{k}=find(U<temp_train);
+end
+
+clear g
+g(1,1)=gramm('x',spike_train,'color',c);
+g(1,1).geom_raster();
+g(1,1).set_title('geom_raster()');
+
+g(1,2)=gramm('x',spike_train,'color',c);
+g(1,2).stat_bin('nbins',25,'geom','line');
+g(1,2).set_title('stat_bin()');
+
+g.set_names('x','Time','y','');
+g.set_title('Visualization of spike densities');
 
 figure;
 g.draw();
